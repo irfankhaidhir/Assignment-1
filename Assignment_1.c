603a202fa5a3b1b765b65c0f7a38206daa06ede3
irfankhaidhir <irfan.khaidhir@gmail.com>
2020-04-24 07:26:09 +0800

    Third commit

--- Assignment_1.c
+++ Assignment_1.c
@@ -1,3 +1,38 @@
+/*
+
+Created by: Irfan
+Function: 
+
+Upon process running, create 2 thread, thread function as describe below:
+
+Thread 1:
+
+Accept user command from command prompt
+Print out message based on user command prompt
+Command supported:
+q : Quit current program (whole program)
+c : create a string buffer
+s : insert user key in string into buffer (created by c)
+Up to you to decide how user key in the desire string
+g : print string buffer (user key in-in string using command s)
+s : change thread 2 updating time
+f : flag high
+
+Thread 2:
+
+Perform the following function periodically (time control by ‘s’). Default to every seconds
+Increase the first character of string buffer : e.g. a → b, or A→ B
+
+
+Instructions:
+
+   1. Create string buffer
+   2. Input string
+   3. Print out buffer (Optional)
+   4. Update time for Thread 2 (Optional)
+   5. Set flag to HIGH to begin increment of string
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -5,15 +40,17 @@
 #include <unistd.h>
 
 unsigned int changetime = 1;
+char *buffer;
 int flag = 0;
-pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+pthread_t t1,t2;
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER, m1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 void *myfunc1(void *ptr)
 {
-   char input[0],*buffer;
+   char input[0];
    unsigned int *newtime = (unsigned int*)malloc(sizeof(unsigned int));
-   printf("Please enter a command\n q:Quit current program\n c:Create string buffer\n s:Insert user key in string into buffer\n g:Print string buffer\n t:Change Thread 2 updating time\n");
+   printf("Please enter a command\n q:Quit current program\n c:Create string buffer\n s:Insert user key in string into buffer\n g:Print string buffer\n t:Change Thread 2 updating time\n f:Set flag to high\n");
    while(1)
    {
 
@@ -22,7 +59,7 @@ void *myfunc1(void *ptr)
    switch(input[0])
    {
       case 'q':
-      return EXIT_SUCCESS;
+      exit(42);
       break;
 
       case 'c':
@@ -51,15 +88,18 @@ void *myfunc1(void *ptr)
       {      
          printf("Enter updated time\n");        
          scanf("%u",newtime);
-         pthread_mutex_lock(&m);
          changetime = *newtime;
-         pthread_mutex_unlock(&m);
+         free(newtime);
          break;
       }
 
       case 'f':
-      {       
-         flag = 0;
+      {
+         pthread_mutex_lock(&m1);      
+         flag = 1;
+         printf("Please wait...\n");
+         pthread_mutex_unlock(&m1);
+         //sleep(10000);
          break;
       }
 
@@ -69,11 +109,8 @@ void *myfunc1(void *ptr)
 
 void *myfunc2(void *ptr)
 {
-   char string = 'a';
-   pthread_mutex_lock(&m);
+   //char string = 'a';
 
-   while(string != '{')
-   {
       if(flag == 0)
       {
          pthread_cond_wait(&cond,&m);  
@@ -81,25 +118,24 @@ void *myfunc2(void *ptr)
 
       else
       {
-      
-      printf("%c\n",string);
-      string+=1;
-  
-      sleep(changetime);  
-
+         pthread_mutex_lock(&m);           
+         printf("%s\n",buffer);    
+         buffer[0]+=1;                  
+         sleep(changetime); 
+         pthread_mutex_unlock(&m); 
       }
- 
-   }
-   pthread_mutex_unlock(&m);     
+
+      //pthread_join(t1,NULL);        
 }
 
 int main()
 {
-   pthread_t t1,t2;
-   pthread_create(&t1,NULL,myfunc1,"Thread1");
 
-   while(1)
+   pthread_create(&t1,NULL,myfunc1,"Thread1");
+   pthread_create(&t2,NULL,myfunc2,"Thread2");   
+   while (1)
    {
-      pthread_create(&t2,NULL,myfunc2,"Thread2");
+      pthread_create(&t2,NULL,myfunc2,"Thread2");         
    }
+
 }
