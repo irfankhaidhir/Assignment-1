ce12b08dcc8f9fcb88c90bc73164fbb4d712dcdc
irfankhaidhir <irfan.khaidhir@gmail.com>
2020-04-30 19:19:21 +0800

    NInth commit

--- Assignment_1.c
+++ Assignment_1.c
@@ -15,7 +15,6 @@ c : create a string buffer
 s : insert user key in string into buffer (created by c)
 g : print string buffer (user key in-in string using command s)
 t : change thread 2 updating time
-f : flag HIGH
 
 Thread 2:
 
@@ -29,7 +28,6 @@ Instructions:
    2. Input string
    3. Print out buffer (Optional)
    4. Update time for Thread 2 (Optional)
-   5. Set flag to HIGH to begin increment of string
 */
 
 #include <stdio.h>
@@ -40,7 +38,6 @@ Instructions:
 
 unsigned int changetime = 1;
 char *buffer;
-int flag = 0;
 pthread_t t1,t2;
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER, m1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
@@ -49,7 +46,7 @@ void *myfunc1(void *ptr) //Thread t1
 {
    char input[0];
    unsigned int *newtime = (unsigned int*)malloc(sizeof(unsigned int));
-   printf("Please enter a command\n q:Quit current program\n c:Create string buffer\n s:Insert user key in string into buffer\n g:Print string buffer\n t:Change Thread 2 updating time\n f:Set flag to high\n");
+   printf("Please enter a command\n q:Quit current program\n c:Create string buffer\n s:Insert user key in string into buffer\n g:Print string buffer\n t:Change Thread 2 updating time\n");
    while(1)
    {
 
@@ -63,10 +60,9 @@ void *myfunc1(void *ptr) //Thread t1
 
       case 'c':
       {
-         buffer = "New Data";
+         buffer=(char*)malloc(sizeof(char));
          printf("String buffer created\n");
          break;
-
       }
 
       case 's':
@@ -92,45 +88,35 @@ void *myfunc1(void *ptr) //Thread t1
          break;
       }
 
-      case 'f':
-      {
-         pthread_mutex_lock(&m1);      
-         flag = 1;
-         printf("Please wait...\n");
-         pthread_mutex_unlock(&m1);
-         break;
-      }
-
    }
    }
 }
 
 void *myfunc2(void *ptr) //Thread t2
 {
-
-      if(flag == 0)
+   while(1)
+   {
+      if (buffer)
       {
-         pthread_cond_wait(&cond,&m);  
-      }
-
-      else
-      {
-         pthread_mutex_lock(&m);           
-         printf("%s\n",buffer);    
-         buffer[0]+=1;                  
+         printf("%s\n",buffer);
+         pthread_mutex_lock(&m);              
+         buffer[0]+=1;
+         pthread_mutex_unlock(&m);                            
          sleep(changetime); 
-         pthread_mutex_unlock(&m); 
+      
       }
-     
+
+   }
 }
 
 int main()
 {
 
    pthread_create(&t1,NULL,myfunc1,"Thread1");  
+   pthread_create(&t2,NULL,myfunc2,"Thread2");   
    while (1)
    {
-      pthread_create(&t2,NULL,myfunc2,"Thread2");         
+               
    }
 
 }
